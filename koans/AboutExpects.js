cb1eec7a4a5af8bb3d932eba505baab298214a25
Sara Brandes <smbrandes17@gmail.com>
2017-12-18 16:55:48 -0800

    solved 3rd

--- koans/AboutExpects.js
+++ koans/AboutExpects.js
@@ -15,9 +15,9 @@ describe("About Expects", function() {
 
   // Some ways of asserting equality are better than others.
   it("should assert equality a better way", function() {
-	  var expectedValue = FILL_ME_IN;
+	  var expectedValue = 2;
 	  var actualValue = 1 + 1;
-
+    var expect = chai.expect;
   // toEqual() compares using common sense equality.
 	  expect(actualValue).toEqual(expectedValue);
   });
